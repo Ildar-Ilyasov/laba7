8968b95c9e6500122cab01aadec866531f3ac414
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-03-03 09:32:05 +0300

    init commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,26 +19,19 @@ class Calculator {
         return (a + b) / (a - b);
     }
 
-    int lvl3() {
-        int y = (a * x / b);
-        int result = 1;
-        if (b != 0) {
-            if (y > 0 && y % 1 == 0) {
-                for (int i = 1; i <= y; i++) {
-                    result = result * i;
-                }
+    void lvl3(){
+        if (b != 0){
+            int y = 1;
+            for (int i = 1; i<= (a*x)/b; i++){
+                y = y*i;
             }
-            else{
-                return y;
-            }
+            System.out.println(y);
         }
-        else {
-            return 0;
+        else{
+            System.out.println("Error");
         }
-        return y;
     }
 }
-
 public class Main {
 
     public static void main(String[] args) {
@@ -46,9 +39,8 @@ public class Main {
         Calculator calc = new Calculator(in.nextInt(),in.nextInt(),in.nextInt());
         int y1 = calc.lvl1();
         double y2 = calc.lvl2();
-        int y3 = calc.lvl3();
         System.out.println(y1);
         System.out.println(y2);
-        System.out.println(y3);
+        calc.lvl3();
     }
 }
